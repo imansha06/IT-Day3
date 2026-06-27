325417d90012fe3c9966664462020a1d0f965a81
Divya Imansha <imanshaaa06@gmail.com>
2026-06-27 15:10:10 +0530

    commit 3

--- js/app.js
+++ js/app.js
@@ -1,70 +1,71 @@
-let name = "Saman";
-// let name = "Saman";
+// } else {
+//     console.log("false");
+// }
 
-let age = 18;
-// let age = 18;
+// -------------------------------------------
 
-name = "ranil";
-// name = "ranil";
-
-console.log(name);
-console.log(age);
-// console.log(name);
-// console.log(age);
-
-alert("Hello icet");
-// alert("Hello icet");
-
-
-// String name = "10";
-// String name = "10";
-
-// ---------------------------------
-
-// let name = "saman";
-// let age = 18;
-// let isAfterAL = false;
-// let customers = [];
-// let customer = {};
-// let salary = 7500.0;
-
-// console.log(typeof name);
-
-// --------------------------------------------
-//  arithmetic operators
-
-// + | - | * | /
+// if else statement
 
 // let x = 10;
 // let y = 20;
 
-// let z = x / y;
-
-// console.log(z);
-
-// -------------------------------------------
-
-//  comparison operators
-
-// > | < | >= | <= | == | !=
-// >== | <== | === | !==
-
-
-// let x = 11;
-
-// let y = "11";
-
-// console.log(x===y);
-
-
-// -------------------------------------------
-
-//  logical operators
-
-//  && | || | !
-
-// if (!false) {
-//     console.log("true");
+// if (x > y) {
+//     console.log("x is greater than y");
+// } else if (x < y) {
+//     console.log("x is less than y");
 // } else {
-//     console.log("false");
+//     console.log("x is equal to y");
 // }
+
+// -------------------------------------------
+
+// switch statement
+
+// let day = 1;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+// -------------------------------------------
+
+// for loop
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// -------------------------------------------
+
+// while loop
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// -------------------------------------------
+
+// do while loop
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+
+// -------------------------------------------
+
+// function
