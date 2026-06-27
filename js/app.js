793a6111367c351fdf72376af165e445ff6bd4bc
Divya Imansha <imanshaaa06@gmail.com>
2026-06-27 15:22:55 +0530

    commit 4

--- js/app.js
+++ js/app.js
@@ -1,71 +1,19 @@
-// } else {
-//     console.log("false");
-// }
-
-// -------------------------------------------
-
-// if else statement
-
-// let x = 10;
-// let y = 20;
-
-// if (x > y) {
-//     console.log("x is greater than y");
-// } else if (x < y) {
-//     console.log("x is less than y");
-// } else {
-//     console.log("x is equal to y");
-// }
-
-// -------------------------------------------
-
-// switch statement
-
-// let day = 1;
-
-// switch (day) {
-//     case 1:
-//         console.log("Monday");
-//         break;
-//     case 2:
-//         console.log("Tuesday");
-//         break;
-//     case 3:
-//         console.log("Wednesday");
-//         break;
-//     default:
-//         console.log("Invalid day");
-// }
-
-// -------------------------------------------
-
-// for loop
-
-// for (let i = 0; i <= 10; i++) {
-//     console.log(i);
-// }
-
-// -------------------------------------------
-
-// while loop
-
-// let i = 0;
-// while (i <= 10) {
-//     console.log(i);
-//     i++;
-// }
-
-// -------------------------------------------
-
-// do while loop
-
-// let i = 0;
-// do {
-//     console.log(i);
-//     i++;
-// } while (i <= 10);
-
 
 // -------------------------------------------
 
 // function
+// function
+
+function add(x, y) {
+    return x + y;
+}
+
+let result = add(10, 20);
+console.log(result);
+
+
+// public static String add(int x, int y) {
+//     return x + y;
+// }
+
+// -------------------------------------------
